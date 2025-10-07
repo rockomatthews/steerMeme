f4eeec23717bb2f316354201cac4b7a70dd491c2
rockomatthews <rob@robmatthe.ws>
2025-10-07 15:58:48 -0600

    fix imessage preview

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,8 +7,24 @@ import Header from '@/components/Header'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'SteerMeme Miner',
-	description: 'Mine faster with more Randy'
+    title: 'randymining.com - More Randy, Faster Mining',
+    description: 'The more you stake, the faster you mine.',
+    openGraph: {
+        title: 'randymining.com - More Randy, Faster Mining',
+        description: 'The more you stake, the faster you mine.',
+        url: 'https://www.randymining.com',
+        siteName: 'randymining.com',
+        images: [
+            { url: 'https://www.randymining.com/imessagepreview.png', width: 1200, height: 630, alt: 'Randy Mining' }
+        ],
+        type: 'website'
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'randymining.com - More Randy, Faster Mining',
+        description: 'The more you stake, the faster you mine.',
+        images: ['https://www.randymining.com/imessagepreview.png']
+    }
 }
 
 export default function RootLayout({
